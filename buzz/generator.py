a6c1a857665f8ff521d5ed4edaa0fa9711ae19a2
kandrv2007 <30438332+kandrv2007@users.noreply.github.com>
2017-08-03 17:28:56 +0300

    error

--- buzz/generator.py
+++ buzz/generator.py
@@ -11,7 +11,7 @@ def sample(l, n = 1):
     if n == 1:
         return result[0]
     return result
-
+newcomment
 def generate_buzz():
     buzz_terms = sample(buzz, 2)
     phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
